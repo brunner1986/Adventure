e14b5d1e599e1b9e7918a8ec006171b40e9b2d08
Thomas Brunner <thomasbrunner2@gmx.at>
2020-04-06 19:22:51 +0200

    added prefabs and collision

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+
+    Rigidbody2D rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,10 +18,12 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal * Time.deltaTime; 
-        position.y = position.y + 0.1f * vertical * Time.deltaTime;
-        transform.position = position;
+
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 5f * horizontal * Time.deltaTime; 
+        position.y = position.y + 5f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
         
     }
 }
